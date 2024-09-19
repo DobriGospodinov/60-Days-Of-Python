7d69ffb266b3b56b3708ded50168a89eeafe9886
dobri <dobri@abv.bg>
2024-09-19 15:30:44 +0300

    Implemented 'Edit' button functionality.

--- To-Do-App-GUI/to-do-gui.py
+++ To-Do-App-GUI/to-do-gui.py
@@ -5,17 +5,13 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
-
-# The label and input box below are displayed on the same line, if they are part of the same list [[label, input_box]].
-
-# If they are in two different lists, like [[label], [input_box]], they would have been displayed above one another.
-
-# So with the current layout=[[label], [input_box, add_button]], the label is displayed on the first row, then
-# the input box and 'add' button are displayed on the second row.
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 15))
 
 # This while loop makes it so that the window doesn't close after pressing the 'Add' button
@@ -29,6 +25,19 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
 
         case sg.WIN_CLOSED:
             break
