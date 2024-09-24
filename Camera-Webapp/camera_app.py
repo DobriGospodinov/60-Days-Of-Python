08a0d416b626a3cb649e78d34e19583df797d046
dobri <dobri@abv.bg>
2024-09-24 18:38:20 +0300

    added option to upload photo to the camera app

--- Camera-Webapp/camera_app.py
+++ Camera-Webapp/camera_app.py
@@ -1,11 +1,23 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to Grayscale Converter")
+
 with st.expander("Start Camera"):
     photo = st.camera_input("Camera")
+
+with st.expander("Upload Image"):
+    pc_photo = st.file_uploader("")
 
 if photo:
 
     img = Image.open(photo)
     grey_img = img.convert("L")
     st.image(grey_img)
+
+if pc_photo:
+
+    img = Image.open(pc_photo)
+    grey_img = img.convert("L")
+    st.image(grey_img)
+
