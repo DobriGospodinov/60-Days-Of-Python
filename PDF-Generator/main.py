4a90d9dae0ef252c363a6283fcf5dd9a0a8db160
dobri <dobri@abv.bg>
2024-09-28 20:28:22 +0300

    Added functionality to the pdf generator app, which adds a preset number of pages after each title.

--- PDF-Generator/main.py
+++ PDF-Generator/main.py
@@ -9,11 +9,14 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index,row in df.iterrows():
-    pdf.add_page()
 
-    pdf.set_font(family="Times", size=24)
-    pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(x1=10, y1=22, x2=200, y2=22)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.set_font(family="Times", size=24)
+        pdf.set_text_color(100, 100, 100)
+        if i == 0:
+            pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+            pdf.line(x1=10, y1=22, x2=200, y2=22)
+
 
 pdf.output("output.pdf")
