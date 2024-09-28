3b5031372ba232be69b4967170641e98adc2eff5
dobri <dobri@abv.bg>
2024-09-29 01:56:20 +0300

    Personal Assignment 1 code refactors

--- Personal-Assignments/1-Write-From-Txt-To-Pdf/main.py
+++ Personal-Assignments/1-Write-From-Txt-To-Pdf/main.py
@@ -9,8 +9,9 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
-    header = filepath.split(".")[0]
-    pdf.cell(w=50, h=8, txt=header.capitalize(), align="L", ln=1)
+    title = filepath.split(".")[0]
+    title = title.capitalize()
+    pdf.cell(w=50, h=8, txt=title, align="L", ln=1)
     pdf.set_font(family="Times", size=12, style="B")
     with open(filepath, 'r') as file:
         content = file.readlines()
