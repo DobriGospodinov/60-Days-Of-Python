42856b3485abfb1a4bc7040a15b2f593265fdac3
dobri <dobri@abv.bg>
2024-10-04 14:54:22 +0300

    PA-4-Dynamic-Plotting-App: Renamed var for better understandability

--- Personal-Assignments/4-Dynamic-Plotting-App/main.py
+++ Personal-Assignments/4-Dynamic-Plotting-App/main.py
@@ -3,17 +3,17 @@ import pandas as pd
 import plotly.express as px
 
 st.title("In Search for Happines")
-option1 = st.selectbox("Select the data for the X-axis", ("GDP", "Happiness", "Generosity"))
-option2 = st.selectbox("Select the data for the Y-axis", ("GDP", "Happiness", "Generosity"))
-st.subheader(f"{option1} and {option2}")
+option_x = st.selectbox("Select the data for the X-axis", ("GDP", "Happiness", "Generosity"))
+option_y = st.selectbox("Select the data for the Y-axis", ("GDP", "Happiness", "Generosity"))
+st.subheader(f"{option_x} and {option_y}")
 
 df = pd.read_csv("happy.csv")
 
-data1 = option1.lower()
-data2 = option2.lower()
+data_x = option_x.lower()
+data_y = option_y.lower()
 
-x = df[f"{data1}"].tolist()
-y = df[f"{data2}"].tolist()
+x = df[f"{data_x}"].tolist()
+y = df[f"{data_y}"].tolist()
 
-figure = px.scatter(x=x, y=y, labels={"x": f"{option1}", "y": f"{option2}"})
+figure = px.scatter(x=x, y=y, labels={"x": f"{option_x}", "y": f"{option_y}"})
 st.plotly_chart(figure)
