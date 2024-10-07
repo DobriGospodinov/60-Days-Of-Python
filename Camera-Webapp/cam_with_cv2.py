2a9e3629e50416ce31b7eed258092800b56a07f0
dobri <dobri@abv.bg>
2024-10-07 16:14:37 +0300

    Camera-Webapp: added an alternative version that uses cv2

---
+++ Camera-Webapp/cam_with_cv2.py
@@ -0,0 +1,33 @@
+import cv2
+import streamlit as st
+import time
+from datetime import datetime
+
+
+st.title("Motion Detector")
+start = st.button("Start Camera")
+
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(0)
+    time.sleep(1)
+
+    while True:
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Get current day and date
+        now = datetime.now()
+        day_of_week = now.strftime("%A")  # Full day name (e.g., "Monday")
+        date_str = now.strftime("%Y-%m-%d")  # Date in "YYYY-MM-DD" format
+
+        cv2.putText(img=frame,text=day_of_week, org=(10, 20),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                    color=(20, 100, 200), thickness=2, lineType=cv2.LINE_AA)
+
+        cv2.putText(img=frame,text=date_str, org=(10, 40),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1,
+                    color=(20, 100, 200), thickness=2, lineType=cv2.LINE_AA)
+
+        streamlit_image.image(frame)
