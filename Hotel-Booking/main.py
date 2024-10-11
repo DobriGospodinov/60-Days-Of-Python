96be2ba0b6dae88bcaab1028ab038905d158ad2e
dobri <dobri@abv.bg>
2024-10-11 17:51:16 +0300

    Hotel-Booking: Implemented credit card functionality

--- Hotel-Booking/main.py
+++ Hotel-Booking/main.py
@@ -3,6 +3,10 @@ import pandas as pd
 df = pd.read_csv("hotels.csv", dtype={"id": str}) # dtype ensures that the "id" column is loaded as a
                                                                 # string, even if it contains numbers
 
+# export credit card data as list of dictionaries
+df_cards = pd.read_csv("cards.csv", dtype=str).to_dict(orient="records")
+df_cards_security = pd.read_csv("card_security.csv", dtype=str)
+
 
 class Hotel:
     def __init__(self, hotel_id):
@@ -20,7 +24,7 @@ class Hotel:
     def book(self):
         """Book a hotel by changing it's availability to no"""
         df.loc[df["id"] == self.hotel_id, "available"] = "no"
-        df.to_csv("hotels.csv", index=False) #index=False: By default, pandas writes the DataFrame's
+        df.to_csv("hotels.csv", index=False) # index=False: By default, pandas writes the DataFrame's
                                                        # row indices (the index) to the CSV file as the first column.
                                                        # Setting index=False tells pandas not to write the index to the
                                                        # CSV file. This means the CSV will only include the DataFrame's
@@ -42,16 +46,44 @@ class Ticket:
         return content
 
 
-# main loop:
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
 
+    def validate(self, expiration, holder, cvc):
+        card_data = {"number": self.number, "expiration": expiration, "holder": holder, "cvc": cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
+
+
+# SecureCreditCard (child) is inheriting from the CreditCard (parent) class:
+class SecureCreditCard(CreditCard):
+    def authenticate(self, given_password):
+        password = df_cards_security.loc[df_cards_security["number"] == self.number, "password"].squeeze()
+        if password == given_password:
+            return True
+        else:
+            return False
+
+
+# MAIN LOOP:
 print(df)
 hotel_ID = input("Enter the id of the hotel: ")
 hotel = Hotel(hotel_ID)
 
 if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = Ticket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+    credit_card = SecureCreditCard(number="1234567890123456")
+    if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
+        if credit_card.authenticate(given_password="mypass"):
+            hotel.book()
+            name = input("Enter your name: ")
+            reservation_ticket = Ticket(customer_name=name, hotel_object=hotel)
+            print(reservation_ticket.generate())
+        else:
+            print("Credit card authentication failed.")
+    else:
+        print("There was a problem with your payment.")
 else:
     print("Hotel isn't free.")
